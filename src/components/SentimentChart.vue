41872291ef6165d99c0b0a1eda5c49c078592d7e
Papa bassirou ndiaye <pbnlgk@gmail.com>
2021-08-23 20:45:16 -0300

    fianl

---
+++ src/components/SentimentChart.vue
@@ -0,0 +1,25 @@
+<template>
+  <canvas :id="id" width="200" height="200"></canvas>
+</template>
+
+<script>
+import Chart from "chart.js";
+import sentimentData from "@/composables/sentiment-data.js"
+export default {
+    props: {
+        score: {
+            required: true 
+        },
+        id: {
+            required: true,
+            type: Number
+        },
+    },
+    mounted() {
+        const ctx = document.getElementById(this.id).getContext('2d');
+        new Chart(ctx, sentimentData(this.score));
+    }
+};
+</script>
+
+<style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,87 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <p class="text-muted">{{ count }} results</p>
+    <div v-for="(tweet, index) in tweets" :key="index" class="block">
+      <div class="tweet" v-show="visible">
+        <div class="tweet__header">
+          <span>{{ tweet.emoji }} {{ tweet.sentiment }}</span>
+          <button @click="zip">switch</button>
+        </div>
+        <p>{{ tweet.text }}</p>
+      </div>
+      <sentiment-chart :score="tweet.score" :id="index" v-show="!visible" />
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import axios from "axios";
+import { ref, onMounted } from "vue";
+import SentimentChart from "@/components/SentimentChart.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
+    SentimentChart,
+  },
+  setup() {
+    const tweets = ref([]);
+    const count = ref(0);
+    const baseUrl = ref("http://localhost:5000/api/v1/macky_sall");
+    const visible = ref(false)
+
+    const getTweet = async () => {
+      const res = await axios.get(baseUrl.value);
+      tweets.value = res.data.data;
+      count.value = res.data.count;
+    };
+
+    const zip = () => {
+      this.visible.value = !this.visible.value;
+    }
+
+    onMounted(getTweet);
+
+    return { tweets, getTweet, baseUrl, visible, zip, count };
   },
 };
 </script>
+
+<style scoped>
+
+.block {
+  width: 90vw;
+}
+.tweet {
+  border-radius: 5px;
+  box-shadow: 0 0.3rem 0.6rem rgba(0, 0, 0, 0.2);
+  border: 2px solid transparent;
+  margin-bottom: 3rem;
+  padding: 1rem 0.6rem;
+  cursor: pointer;
+}
+
+.tweet__header {
+  display: flex;
+  justify-content: space-between;
+  font-weight: bold;
+}
+
+@media screen and (min-width: 1024px) {
+  .block {
+    width: 50vw;
+  }
+}
+
+.home {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.text-muted {
+  color: rgb(161, 159, 159);
+}
+</style>
